d4f015d26a08001b1cca2f6058a025e78e8c0b08
Wesley Dashner <wesleydashner@wesleys-macbook-pro.local>
2018-10-02 15:45:56 -0600

    Initial Commit

---
+++ Tic Tac Toe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tic Tac Toe
+//
+//  Created by Wesley Dashner on 10/2/18.
+//  Copyright © 2018 Wesley Dashner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
